d278d436661498e0351f7af73722e6b50f6a9fd3
Louis P <64967788+LouisPerre@users.noreply.github.com>
2021-11-19 12:41:22 +0100

    :hammer: FIX JS

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ window.addEventListener('resize', () => {
   });
 
 
-if (window.location.pathname == "/Portfolio/project.html" or "/project.html") {
+if (window.location.pathname == "/Portfolio/project.html" or window.location.pathname == "/project.html") {
     const projectTitle = document.querySelector(".projectTitle")
     const firstP = document.querySelector(".firstP")
     const secondP = document.querySelector(".secondP")
@@ -65,7 +65,7 @@ if (window.location.pathname == "/Portfolio/project.html" or "/project.html") {
 
 }
 
-if (window.location.pathname == "/Portfolio/competence.html" or "/competence.html") {
+if (window.location.pathname == "/Portfolio/competence.html" or window.location.pathname == "/competence.html") {
     const projectTitle = document.querySelector(".projectTitle")
     const CompTitle = document.querySelector(".CompTitle")
     const firstP = document.querySelector(".firstP")
